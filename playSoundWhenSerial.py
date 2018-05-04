6ef98c600bdace432a5d7bfcdb431e3643563fd7
crocagiles <gilesholbrow@gmail.com>
2018-05-04 07:17:18 +0000

    Changes to simple audio module

--- playSoundWhenSerial.py
+++ playSoundWhenSerial.py
@@ -8,31 +8,40 @@ Created on Mon Apr 23 20:25:00 2018
 import time
 import wave 
 import serial
-from playsound import playsound
 
-import pygame
+import simpleaudio as sa
+wave_obj = sa.WaveObject.from_wave_file("/home/pi/CODE/NeoPixelStuff/TIE-Fire.wav")
+play_obj = wave_obj.play()
+play_obj.wait_done()
 
-ser = serial.Serial('/dev/tty.usbmodem1421', 9600)
+#pygame.mixer.init(48000, -16, 1, 1024)
+#pygame.mixer.music.load("/Users/gholbrow/Downloads/TIE-Fire.wav")
+#pygame.mixer.music.load("/home/pi/CODE/NeoPixelStuff/TIE-Fire.wav")
+#pygame.mixer.music.play()   
+
+
+
+ser = serial.Serial('/dev/ttyACM0', 9600)
 while True:
     derp = ser.readline()
-    derp2 = derp.decode('utf-8') 
+    derp2 = derp.decode('utf-8')
+    
     if 'tie' in derp2:
         print(derp2)
-        
-        pygame.mixer.init()
-        pygame.mixer.music.load("/Users/gholbrow/Downloads/TIE-Fire.wav")
-        pygame.mixer.music.play()   
+        wave_obj = sa.WaveObject.from_wave_file("/home/pi/CODE/NeoPixelStuff/TIE-Fire.wav")
+        play_obj = wave_obj.play()
+        play_obj.wait_done()
               
     if 'Blaster' in derp2:
         print(derp2)
-        
-        pygame.mixer.init()
-        pygame.mixer.music.load("/Users/gholbrow/Downloads/Blaster-Imperial.wav")
-        pygame.mixer.music.play()     
+
+        wave_obj = sa.WaveObject.from_wave_file("/home/pi/CODE/NeoPixelStuff/Blaster-Imperial.wav")
+        play_obj = wave_obj.play()
+        play_obj.wait_done()        
         
     if 'cop' in derp2:
         print(derp2)
-        
-        pygame.mixer.init()
-        pygame.mixer.music.load("/Users/gholbrow/Downloads/414208__jacksonacademyashmore__airhorn.wav")
-        pygame.mixer.music.play()         
+
+        wave_obj = sa.WaveObject.from_wave_file("/home/pi/CODE/NeoPixelStuff/414208__jacksonacademyashmore__airhorn.wav")
+        play_obj = wave_obj.play()
+        play_obj.wait_done()         
